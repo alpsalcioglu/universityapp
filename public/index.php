96aa57002604f197d51d5d0f293b9c8675ca5fbb
Alp Salcıoğlu <146808721+alpsalcioglu@users.noreply.github.com>
2025-02-24 18:54:15 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,8 +3,10 @@
 
 use DI\Container;
 use Slim\Factory\AppFactory;
-
-
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Exception\HttpNotFoundException;
+use Slim\Views\PhpRenderer;
 
 require __DIR__ . '/../vendor/autoload.php';
 require '../helpers.php';
@@ -19,5 +21,23 @@ $app = AppFactory::create();
 
 $routes = require __DIR__ . '/../config/routes.php';
 $routes($app);
+
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$errorMiddleware->setErrorHandler(
+    Slim\Exception\HttpNotFoundException::class,
+    function (Request $request, Throwable $exception, bool $displayErrorDetails, bool $logErrors, bool $logErrorDetails) use ($app) {
+        $response = $app->getResponseFactory()->createResponse();
+
+
+        ob_start();
+        require '../templates/404view.php';
+        $html = ob_get_clean();
+
+
+        $response->getBody()->write($html);
+        return $response->withStatus(404);
+    }
+);
 
 $app->run();
